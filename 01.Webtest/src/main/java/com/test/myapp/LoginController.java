03c711dd0ae6e15d9881bba13a5c7ce8a29b7c22
slvr.png <rmsp7223123@naver.com>
2023-12-11 12:49:53 +0900

    ++

---
+++ 01.Webtest/src/main/java/com/test/myapp/LoginController.java
@@ -0,0 +1,10 @@
+package com.test.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+}
---
+++ rxo.sql
@@ -0,0 +1,6 @@
+INSERT INTO members (user_id, username, user_password)
+VALUES ('testrxo1', 'testrxo', '0000');
+
+select * from members;
+
+commit;
