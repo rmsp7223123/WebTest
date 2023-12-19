94c7e110de81189a04ca4760808ebf078957088f
slvr.png <rmsp7223123@naver.com>
2023-12-19 14:36:20 +0900

    +

--- 01.Webtest/src/main/java/com/test/myapp/BoardController.java
+++ 01.Webtest/src/main/java/com/test/myapp/BoardController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import rxo.board.AttachmentVO;
+import rxo.board.BoardVO;
+
 @Controller
 public class BoardController {
 	@Autowired
@@ -14,6 +17,15 @@ public class BoardController {
 	
 	@RequestMapping("new")
 	public String newWrite() {
+		return "new";
+	}
+	
+	@RequestMapping("insert")
+	public String insertBoard(BoardVO vo, AttachmentVO vo2) {
+		sql.insert("board.write", vo);
+		if (vo2 != null && vo2.get != null && !vo2.getFilePath().isEmpty()) {
+	        sql.insert("board.attachFile", vo2);
+	    }
 		return "new";
 	}
 
--- 01.Webtest/src/main/java/rxo/board/AttachmentVO.java
+++ 01.Webtest/src/main/java/rxo/board/AttachmentVO.java
@@ -1,5 +1,10 @@
 package rxo.board;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AttachmentVO {
 	int attachmentId, boardId;
 	String filePath;
